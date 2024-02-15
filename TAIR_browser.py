a98e1944f80c00f16da3e96799c409d90597277c
Alejandro Ruiz Bayón <alejandro_bayon@hotmail.com>
2024-02-15 15:53:21 +0100

    Write response to file
    The response of the webpage is stored in a TXT file for further uses.

--- TAIR_browser.py
+++ TAIR_browser.py
@@ -1,7 +1,13 @@
 import requests
 
-response = requests.get("https://www.arabidopsis.org/servlets/")
+response = requests.get("https://www.arabidopsis.org/servlets/TairObject?id=30616&type=locus")
 
 print(response.request.url)
 print(response.request.body)
 print(response)
+
+file = open("prueba.txt", "w")
+
+file.write(response.text)
+
+file.close()
