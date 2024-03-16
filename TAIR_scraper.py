086ae025dca7bd0f9df85d0cbeda5efd0f121f35
Alejandro Ruiz Bayón <alejandro_bayon@hotmail.com>
2024-03-16 19:44:37 +0100

    Cleaning of the HTML
    Added the functionality to create a new file with the HTML of the page cleaned from HTML tags to improve its readability.

--- TAIR_scraper.py
+++ TAIR_scraper.py
@@ -1,23 +1,29 @@
 from urllib.request import urlopen
 import time
+from bs4 import BeautifulSoup
 
 time1 = time.perf_counter()
 
 print("Obtaining HTML")
 
 page = urlopen("https://www.arabidopsis.org/servlets/TairObject?id=6534023021&type=germplasm")
-html_bytes = page.read()
-html = html_bytes.decode("utf-8")
+html = page.read().decode("utf-8")
 
-file = open("Prueba.txt", "w")
+file = open("HTML_text.txt", "w")
 file.write(html)
 file.close()
 
 time2 = time.perf_counter()
 
 total_time = time2 - time1
-
 print(total_time)
+
+soup = BeautifulSoup(html, "html.parser")
+text = soup.get_text()
+
+file = open("clean_text.txt", "w")
+file.write(text)
+file.close()
 
 #url1 = http://olympus.realpython.org/profiles/aphrodite
 #url2 = https://www.arabidopsis.org/servlets/TairObject?id=6534023021&type=germplasm
